f7d0ea323e8557c53587414d04a1d5ee874e5f8d
GORDON9839 <44221097+GORDON9839@users.noreply.github.com>
2019-08-03 21:33:59 +0800

    hi

---
+++ resources/views/XSLTTransformation.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class XSLTTransformation{
+    public function __construct($xmlfilename, $xslfilename)
+    {
+        $xml = new DOMDocument();
+        $xml->load($xmlfilename);
+
+        $xsl = new DOMDocument();
+        $xsl->load($xslfilename);
+
+        $proc = new XSLTProcessor();
+        $proc->importStyleSheet($xsl);
+
+        echo $proc->transformToXml($xml);
+    }
+}
